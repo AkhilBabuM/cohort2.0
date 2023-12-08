13860bc4513d4392bd51ff03409c8734084fc09b
AkhilBabu <akhilbabu.mpn@gmail.com>
2023-12-08 19:28:46 +0530

    first trials

--- week1.2/1.js
+++ week1.2/1.js
@@ -1,2 +1,11 @@
-let a = 20;
-print(a);
+let firstname = "akhil"
+let lastname = "babu"
+
+let gender = "m"
+
+for (i=0; i<1000; i++){
+    if(gender==="m")
+        console.log("Welcome "+firstname+" "+lastname+" for "+(i+1)+" time")
+    else if(gender==="f")
+        console.log("Who da female")
+}
